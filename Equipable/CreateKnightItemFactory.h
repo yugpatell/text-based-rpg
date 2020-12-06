6642908d28b9093d46de458589a54d81baff1b92
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-05 19:18:27 -0800

    Update CreateKnightItemFactory.h

--- Equipable/CreateKnightItemFactory.h
+++ Equipable/CreateKnightItemFactory.h
@@ -9,7 +9,7 @@
 class CreateKnightItemFactory : public CreateEquipableFactory{
   public:
     virtual Chestplate* CreateChestArmor(Character * c){
-      FileReader reader("../FileReader/Rarity.txt");
+      FileReader reader("FileReader/Rarity.txt");
       srand(time(NULL));
       int randomVal = (rand() % reader.rarities.size()) + 1;
       string rarity = reader.rarities.at(randomVal - 1);
@@ -19,7 +19,7 @@ class CreateKnightItemFactory : public CreateEquipableFactory{
     }
 
     virtual ArmoredLeggings* CreateLegArmor(Character * c){
-      FileReader reader("../FileReader/Rarity.txt");
+      FileReader reader("FileReader/Rarity.txt");
       srand(time(NULL));
       int randomVal = (rand() % reader.rarities.size()) + 1;
       string rarity = reader.rarities.at(randomVal - 1);
@@ -29,7 +29,7 @@ class CreateKnightItemFactory : public CreateEquipableFactory{
     }
 
     virtual Sword* CreateWeapon(Character * c){
-      FileReader reader("../FileReader/Rarity.txt");
+      FileReader reader("FileReader/Rarity.txt");
       srand(time(NULL));
       int randomVal = (rand() % reader.rarities.size()) + 1;
       string rarity = reader.rarities.at(randomVal - 1);
