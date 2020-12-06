30049959a304f26c2e70ad4301e55f64a41445d9
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-05 22:36:30 -0800

    Update troll.h

--- Mob/troll.h
+++ Mob/troll.h
@@ -2,6 +2,7 @@
 #define __TROLL_H__
 
 #include "mob.h"
+#include <stdlib.h>
 
 class Troll : public Mob {
 public:
@@ -11,15 +12,15 @@ public:
     Troll(int characterlevel) {
         srand(time(NULL));
     	hp = 2 * characterlevel + rand() % (5 * characterlevel / 4);
-	    attack = 2 * characterlevel + rand() % (5 * characterlevel / 4);
-	    defense = 4 * characterlevel + rand() % (5 * characterlevel / 4);
+	attack = 2 * characterlevel + rand() % (5 * characterlevel / 4);
+	defense = 4 * characterlevel + rand() % (5 * characterlevel / 4);
         level = abs(characterlevel - 4 + rand() % 6) + 1;
-	    giveExp = level * 10 * attack / defense;
+	giveExp = level * 10 * attack / defense;
         name = "TROLL";
     }
 
     int attackChar(int charDefense) {
-	    return (((((2 * level / 5) + 2) * (35 + (level * 9 / 10)) * attack) / charDefense) / 50) +2;
+	    return abs((((((2 * level / 5) + 2) * (35 + (level * 9 / 10)) * attack) / charDefense) / 50) +2);
     }
 
 };
