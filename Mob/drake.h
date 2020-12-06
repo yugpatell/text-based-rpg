9070c25ef2b3d6aa871a98580991e197dda57be4
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-05 22:37:08 -0800

    Update drake.h

--- Mob/drake.h
+++ Mob/drake.h
@@ -2,6 +2,7 @@
 #define __DRAKE_H__
 #include <stdlib.h>
 #include <time.h>
+#include <stdlib.h>
 #include "mob.h"
 
 class Drake : public Mob {
@@ -12,15 +13,15 @@ public:
     Drake(int characterlevel){
         srand(time(NULL));
     	hp = 3 * characterlevel + rand() % (5 * characterlevel / 4);
-	    attack = 3 * characterlevel + rand() % (5 * characterlevel / 4);
-	    defense = 2 * characterlevel + rand() % (5*characterlevel / 4);
+	attack = 3 * characterlevel + rand() % (5 * characterlevel / 4);
+	defense = 2 * characterlevel + rand() % (5*characterlevel / 4);
         level = abs(characterlevel - 4 + rand() % 6) + 1;
-	    giveExp = level*10*attack/defense;
-	    name = "DRAKE";
+	giveExp = level*10*attack/defense;
+	name = "DRAKE";
     }
 
      int attackChar(int charDefense) {
-        return (((((2 * level / 5) + 2) * (35 + (level * 9 / 10)) * attack) / charDefense) / 50) + 2;
+        return abs((((((2 * level / 5) + 2) * (35 + (level * 9 / 10)) * attack) / charDefense) / 50) + 2);
     }
 
 
