f8fd4ccfab2587359d18a7e9ff22e675df49fc2a
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-06 13:23:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 GameRunner * GameRunner::gameInstance = nullptr;
 
 int main() {
-   // FileReader* reader = new FileReader("FileReader/Rarity.txt");
     GameRunner * newGame = newGame->getInstance();
     newGame->startGame();
     return 0;
