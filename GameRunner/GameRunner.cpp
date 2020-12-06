612bf07c1553bf28b2f99480d8f0fd9eb4b141f2
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-06 12:58:35 -0800

    Update GameRunner.cpp

--- GameRunner/GameRunner.cpp
+++ GameRunner/GameRunner.cpp
@@ -351,9 +351,9 @@ void GameRunner::createCharacter() {
     userName = name;
     cout << "\033[1;36m" << name << ", here is a list of the three characters to play as." << "\033[0m" << endl;
     cout << "\033[1;30m-------------------------------------------------------------------------------------------------------------------\033[0m" << endl;
-    cout << "\033[1;33m1) ARCHER - The archer ascends from poor beginnings. As he grew up, he hunted to provide for his family. Afte\nrmonsters plagued the earth and ravaged his village, he promised to avenge his family. The archer specializes in attack.\033[0m" << endl;
-    cout << "\033[1;33m2) MAGE - The mage has mystical origins, coming from an unknown dimension. Seeking to master his skills, he travels\nthe realm, practicing his abstract sorcery. The mage specializes in HP.\033[0m" << endl;
-    cout << "\033[1;33m3) KNIGHT - The knight fell from royalty after being betrayed by the King. Now left with nowhere to go, the Knight\nstruggles to survive in the wilderness. The knight specializes in defense.\033[0m" << endl;
+    cout << "\033[1;33m1) ARCHER - The archer ascends from poor beginnings. As he grew up, he hunted to provide for his family. After\nmonsters plagued the earth and ravaged his village, he promised to avenge his family. The archer specializes in attack.\033[0m\n" << endl;
+    cout << "\033[1;33m2) MAGE - The mage has mystical origins, coming from an unknown dimension. Seeking to master his skills, he travels\nthe realm, practicing his abstract sorcery. The mage specializes in HP.\033[0m\n" << endl;
+    cout << "\033[1;33m3) KNIGHT - The knight fell from royalty after being betrayed by the King. Now left with nowhere to go, the Knight\nstruggles to survive in the wilderness. The knight specializes in defense.\033[0m\n";
     cout << "\033[1;30m-------------------------------------------------------------------------------------------------------------------\033[0m" << endl;
     cout << "\033[1;36mEnter the corresponding number of the character you wish to play as: \033[0m";
     int charNum;
