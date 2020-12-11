cb959e1382e1f4c6872abcf07c7a2db0ac6961b9
ThomasHTran <timtom9801gmail.com>
2020-12-10 21:23:30 -0800

    Made unit tests for armor, weapon, character, and consumable classes

--- Tests/consumable_test.h
+++ Tests/consumable_test.h
@@ -1,10 +1,39 @@
 #ifndef __CONSUMABLE_TEST_HPP__
 #define __CONSUMABLE_TEST_HPP__
 
-#include "gtest/gtest.h"
+#include "../Consumable/Potion.h"
 
+TEST(ConsumableTest, defaultConsumable) {
+  Consumable* consum1 = new Consumable();
+  EXPECT_EQ(consum1->getLevel(), 1);
+  EXPECT_EQ(consum1->getValue(), 5);
+}
 
+TEST(ConsumableTest, customConsumable) {
+  Consumable* consum1 = new Consumable(10, 5, "Epic Consumable");
+  EXPECT_EQ(consum1->getLevel(), 10);
+  EXPECT_EQ(consum1->getValue(), 25);
+  EXPECT_EQ(consum1->getName(), "Epic Consumable");
+}
 
+TEST(ConsumableTest, defaultPotion){
+  Potion* potion1 = new Potion();
+  EXPECT_EQ(potion1->getLevel(), 1);
+  EXPECT_EQ(potion1->getValue(), 5);
+}
 
+TEST(ConsumableTest, customPotion1) {
+  Potion* potion1 = new Potion(20, 1, "Poor Potion");
+  EXPECT_EQ(potion1->getLevel(), 20);
+  EXPECT_EQ(potion1->getValue(), 23);
+  EXPECT_EQ(potion1->getName(), "Poor Potion");
+}
+
+TEST(ConsumableTest, customPotion2) {
+  Potion* potion1 = new Potion(30, 3, "Good Potion");
+  EXPECT_EQ(potion1->getLevel(), 30);
+  EXPECT_EQ(potion1->getValue(), 39);
+  EXPECT_EQ(potion1->getName(), "Good Potion");
+}
 
 #endif
--- Tests/weapon_test.h
+++ Tests/weapon_test.h
@@ -1,8 +1,79 @@
 #ifndef __WEAPON_TEST_HPP__
 #define __WEAPON_TEST_HPP__
 
-#include "gtest/gtest.h"
+#include "../Equipable/Sword.h"
+#include "../Equipable/Staff.h"
+#include "../Equipable/Bow.h"
 
+TEST(WeaponTest, defaultWeapon) {
+  Weapon* weapon1 = new Weapon();
+  EXPECT_EQ(weapon1->getLevel(), 1);
+  EXPECT_EQ(weapon1->getValue(), 5);
+  EXPECT_EQ(weapon1->getRole(), 0);
+  EXPECT_TRUE(!weapon1->isArmor());
+}
+
+TEST(WeaponTest, customWeapon) {
+  Weapon* weapon1 = new Weapon(10, 3, 0);
+  EXPECT_EQ(weapon1->getLevel(), 10);
+  EXPECT_EQ(weapon1->getValue(), 19);
+  EXPECT_EQ(weapon1->getRole(), 0);
+  EXPECT_TRUE(!weapon1->isArmor());
+}
+
+TEST(WeaponTest, defaultSword) {
+  Sword* weapon1 = new Sword();
+  EXPECT_EQ(weapon1->getLevel(), 1);
+  EXPECT_EQ(weapon1->getValue(), 5);
+  EXPECT_EQ(weapon1->getRole(), 0);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), " ");
+}
+
+TEST(WeaponTest, customSword) {
+  Sword* weapon1 = new Sword(4, 4, "Rare Sword");
+  EXPECT_EQ(weapon1->getLevel(), 4);
+  EXPECT_EQ(weapon1->getValue(), 16);
+  EXPECT_EQ(weapon1->getRole(), 0);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), "Rare Sword");
+}
+
+TEST(WeaponTest, defaultStaff) {
+  Staff* weapon1 = new Staff();
+  EXPECT_EQ(weapon1->getLevel(), 1);
+  EXPECT_EQ(weapon1->getValue(), 5);
+  EXPECT_EQ(weapon1->getRole(), 1);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), " ");
+}
+
+TEST(WeaponTest, customStaff) {
+  Staff* weapon1 = new Staff(5, 3, "Good Staff");
+  EXPECT_EQ(weapon1->getLevel(), 5);
+  EXPECT_EQ(weapon1->getValue(), 14);
+  EXPECT_EQ(weapon1->getRole(), 1);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), "Good Staff");
+}
+
+TEST(WeaponTest, defaultBow) {
+  Bow* weapon1 = new Bow();
+  EXPECT_EQ(weapon1->getLevel(), 1);
+  EXPECT_EQ(weapon1->getValue(), 5);
+  EXPECT_EQ(weapon1->getRole(), 2);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), " ");
+}
+
+TEST(WeaponTest, customBow) {
+  Bow* weapon1 = new Bow(7, 1, "Poor Bow");
+  EXPECT_EQ(weapon1->getLevel(), 7);
+  EXPECT_EQ(weapon1->getValue(), 10);
+  EXPECT_EQ(weapon1->getRole(), 2);
+  EXPECT_TRUE(!weapon1->isArmor());
+  EXPECT_EQ(weapon1->getName(), "Poor Bow");
+}
 
 
 #endif
