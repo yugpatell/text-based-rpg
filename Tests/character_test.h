17b4d919dff7ef2c0293dd39b59d654c4a0ba718
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2020-12-10 17:37:41 -0800

    Update character_test.h

--- Tests/character_test.h
+++ Tests/character_test.h
@@ -1,8 +1,6 @@
 #ifndef __CHARACTER_TEST_HPP__
 #define __CHARACTER_TEST_HPP__
 
-#include "gtest/gtest.h"
-#include "..Character/Character.h"
 #include "..Character/Archer.cpp"
 #include "..Character/Knight.cpp"
 #include "..Character/Mage,cpp"
