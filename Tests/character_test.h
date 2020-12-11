8e09348685a1ff2ffb5a207ad7c45ee12849f16d
Thomas Tran <51304839+TimusTrun@users.noreply.github.com>
2020-12-10 21:30:27 -0800

    Update character_test.h

--- Tests/character_test.h
+++ Tests/character_test.h
@@ -1,9 +1,9 @@
 #ifndef __CHARACTER_TEST_HPP__
 #define __CHARACTER_TEST_HPP__
 
-#include "..Character/Archer.cpp"
-#include "..Character/Knight.cpp"
-#include "..Character/Mage,cpp"
+#include "../Character/Archer.cpp"
+#include "../Character/Knight.cpp"
+#include "../Character/Mage,cpp"
 
 TEST(CharacterTest, defaultArcherCharacter){
   Character* char2 = new Archer("Thomas", ARCHER);
