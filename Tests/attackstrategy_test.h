14b84810f02b584f43ae4eb9b328eaff39e5c0bc
scottmez <66883374+scottmez@users.noreply.github.com>
2020-12-10 21:16:58 -0800

    Update attackstrategy_test.h

--- Tests/attackstrategy_test.h
+++ Tests/attackstrategy_test.h
@@ -10,39 +10,39 @@
 #include "../Character/Knight.h"
 #include "../Character/Mage.h""
 
-Test(AttackStrategyTest, AttackStrategyArcherLv1vsdrake) {
+TEST(AttackStrategyTest, AttackStrategyArcherLv1vsdrake) {
   Drake* drake1 = new Drake(1); //def = 3
   Archer* archer1 = new Archer("Scott", ARCHER);
   EXPECT_EQ(archer1->attack(drake1), 3);
 }
 
-Test(AttackStrategyTest, AttackStrategyMageLv1vstroll) {
+TEST(AttackStrategyTest, AttackStrategyMageLv1vstroll) {
   Troll* troll1 = new Troll(1); //def = 4
   Mage* mage1 = new Mage("Scott", MAGE);
   EXPECT_EQ(archer1->attack(troll1), 2);
 }
 
-Test(AttackStrategyTest, AttackStrategyKnightLv1vswolf) {
+TEST(AttackStrategyTest, AttackStrategyKnightLv1vswolf) {
   Wolf* wolf1 = new Wolf(1); //def = 2
   Knight* knight1 = new Knight("Scott", KNIGHT);
   EXPECT_EQ(archer1->attack(wolf1), 2);
 }
 
-Test(AttackStrategyTest, AttackStrategyArcherLv5vswolf) {
+TEST(AttackStrategyTest, AttackStrategyArcherLv5vswolf) {
   Wolf* wolf1 = new Wolf(1); //def = 2
   Archer* archer1 = new Archer("Scott", ARCHER);
   archer1->setcurrXP(550);
   EXPECT_EQ(archer1->attack(wolf1), 41);
 }
 
-Test(AttackStrategyTest, AttackStrategyMageLv5vsyeti) {
+TEST(AttackStrategyTest, AttackStrategyMageLv5vsyeti) {
   Yeti* yeti1 = new Yeti(1); //def = 2
   Mage* mage1 = new Mage("Scott", MAGE);
   mage1->setcurrXP(550);
   EXPECT_EQ(archer1->attack(yeti1), 13);
 }
 
-Test(AttackStrategyTest, AttackStrategyKnightLv5vstroll) {
+TEST(AttackStrategyTest, AttackStrategyKnightLv5vstroll) {
   Troll* troll1 = new Troll(1); //def = 4
   Knight* knight1 = new Knight("Scott", KNIGHT);
   knight1->setcurrXP(550);
